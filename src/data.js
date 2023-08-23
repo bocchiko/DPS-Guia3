21e818466a79faa3f052b68783fbcc5ff3a17bcd
bocchiko <luisbonilla0908@gmail.com>
2023-08-23 10:47:34 -0600

    Guia 3

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Cien años de soledad',
+        price: 100,
+        urlImage: 'https://images.penguinrandomhouse.com/cover/9780525562443',
+        quantity: 1
+    },
+    {
+        id: 2,
+        title: 'El señor de los anillos(Trilogía)',
+        price: 190,
+        urlImage: 'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/thumbs/0f3e9961-b7a8-45e3-b796-8b61d9c635c2/d_1200_1200/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.webp',
+        quantity: 1
+    },
+    {
+        id: 3,
+        title: 'Cuentos de Barro',
+        price: 30,
+        urlImage: 'https://static.cegal.es/imagenes/marcadas/9788483/978848344743.gif',
+        quantity: 1
+    },
+    {
+        id: 4,
+        title: 'Tierra de Infancia',
+        price: 30,
+        urlImage: 'https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg',
+        quantity: 1
+    },
+    {
+        id: 5,
+        title: 'Harry Potter Pack',
+        price: 390,
+        urlImage: 'https://contentv2.tap-commerce.com/cover/large/9789878000473_1.jpg?id_com=1113',
+        quantity: 1
+    }
+];
